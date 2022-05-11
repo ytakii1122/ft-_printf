0e3552a8cf6207fa0a2c0dacdd3b1b8ea03396e4
Yumiko Takii <ytakii@c1r29s1.42tokyo.jp>
2022-05-11 16:47:17 +0900

    output making

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: ytakii </var/mail/ytakii>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/07 17:11:07 by ytakii            #+#    #+#             */
-/*   Updated: 2022/04/21 22:47:53 by ytakii           ###   ########.fr       */
+/*   Updated: 2022/05/11 16:46:24 by ytakii           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,21 +29,45 @@ int	ft_printf(const char *fmt, ...)
 {
 	va_list	ap;
 	//char	c;
-	int	i;
+	//size_t	i;
+	int	return_size;
 
+	//i = 0;
 	va_start(ap, fmt);
-	ft_putstr_fd((char*) fmt, 1);
-	//c = (char)va_arg(ap, int);
-	i = (int)va_arg(ap, int);
-	//printf("%c",i);
-	ft_putchar_fd(i, 1);
+	//while (fmt[i])
+	//{
+		//ft_putstr_fd((char*) fmt, 1);
+		//c = (char)va_arg(ap, int);
+		return_size = (int)va_arg(ap, int);
+		//printf("%c",i);
+
+		ft_putchar_fd(return_size, 1);
+	//}
 	va_end(ap);
-	return (0);
+	return (return_size);//word count include '\n'
 }
 
 int main (void)
 {
-	ft_printf("hoge", '1');
+	ft_printf("%hoge", '1');
 	return (0);
 }
 
+int output_char(char *input , va_list arg)
+{
+	size_t	i;
+	size_t	count
+
+	i = 0;
+	while (input[i] != '\0')
+	{
+	 	if (input[i] == '%')
+			check(input, arg);//flag etc... check
+
+		else
+			ft_putchar_fd(input[i]);
+		i++;
+		count++;
+	}
+	return (count);// word count
+}
---
+++ main.c
@@ -0,0 +1,8 @@
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("aaa\0");
+	return (0);
+}
